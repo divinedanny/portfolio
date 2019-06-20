f54ef0c0baaa9b33100fb19076f0e18e73a463fb
divine <divine@localhost.localdomain>
2019-06-20 19:23:07 +0100

    menu-button, javascript & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+//select DOM classes
+const menuBtn = document.querySelector ('.menu-btn');
+const menu = document.querySelector ('.menu');
+const menuBranding = document.querySelector ('.menu-branding');
+const menuNav = document.querySelector ('.menu-nav');
+const navItems = document.querySelectorAll ('.nav-items');
+
+let showMenu = false;
+
+menuBtn.addEventListener ('click', toggleMenu);
+
+function toggleMenu () {
+  if (!showMenu) {
+    menuBtn.classList.add ('close');
+    menu.classList.add ('show');
+    menuBranding.classList.add ('show');
+    menuNav.classList.add ('show');
+    navItems.forEach (Item => Item.classList.add ('show'));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove ('close');
+    menu.classList.remove ('show');
+    menuBranding.classList.remove ('show');
+    menuNav.classList.remove ('show');
+    navItems.forEach (Item => Item.classList.remove ('show'));
+    showMenu = false;
+  }
+}
